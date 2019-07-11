c31537746ef156f15700d13577b7c3780af1d8e8
DILLON XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEDI@OOCL.COM>
2019-07-11 17:49:11 +0800

    update

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
+  let array=[];
+  let object={};
+  collection.filter((num)=>{
+    
+    object[num]=(object[num]+1)||1;
+  });
+  let x=Object.keys(object);
+  x.filter((num)=>{
+      let obj={
+        key:num,
+        count:object[num]
+      }
+      array.push(obj);
+  });
   return '实现练习要求，并改写该行代码。';
 }
